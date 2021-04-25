00801d0427d28b26f4ba28b7b74191fcccb13962
complito <hardway186@gmail.com>
2021-04-25 17:36:00 +0500

    Изменён метод отрисовки изображений + исправление недочётов

--- WallaceAndGromit/Form1.cs
+++ WallaceAndGromit/Form1.cs
@@ -10,130 +10,65 @@ using System.Windows.Forms;
 
 namespace WallaceAndGromit
 {
+    public enum AnimationDirection
+    {
+        Left,
+        Right,
+        Up,
+        Down,
+        None
+    }
+
     public partial class Form1 : Form
     {
-        private enum AnimationDirection
-        {
-            Left,
-            Right,
-            Up,
-            Down,
-            None
-        }
-        private AnimationDirection currentAnimation = AnimationDirection.None;
-        private AnimationDirection previousAnimation = AnimationDirection.Right;
-        private int currentFrame = 0;
         private bool isPressedAnyKey = false;
-        private Image wallaceImage;
-        private string partPathImage = "D:\\УрФУ\\ЯТП\\WallaceAndGromit\\WallaceAndGromit\\images\\Wallace";
+        private bool toUpdateAnimation = false;
+        private Player wallace;
+        private string partPathImage = "D:\\УрФУ\\ЯТП\\WallaceAndGromit\\WallaceAndGromit\\images\\";
         private string extension = ".png";
-        private PictureBox pictureBox = new PictureBox
-        {
-            Location = new Point(0, 0),
-            SizeMode = PictureBoxSizeMode.StretchImage,
-            Size = new Size(192, 304)
-        };
-        private Timer timerAnimation = new Timer
-        {
-            Interval = 100
-        };
-        private Timer timerMovement = new Timer
-        {
-            Interval = 10
-        };
+        private Timer timerAnimation = new Timer { Interval = 100 };
+        private Timer timerMovement = new Timer { Interval = 10 };
+
         public Form1()
         {
             InitializeComponent();
-            wallaceImage = new Bitmap($"{partPathImage}_Right_{currentFrame}{extension}");
-            pictureBox.Image = wallaceImage;
-            Controls.Add(pictureBox);
+            wallace = new Player(new Size(192, 304), 100, 100, 5);
             timerAnimation.Tick += new EventHandler(UpdateAnimation);
             timerAnimation.Start();
             timerMovement.Tick += new EventHandler(UpdateMovement);
             timerMovement.Start();
             KeyDown += new KeyEventHandler(Keybord);
             KeyUp += new KeyEventHandler(FreeKey);
-        }
-
-        private void FreeKey(object sender, KeyEventArgs e)
-        {
-            switch (e.KeyCode.ToString())
-            {
-                case "A":
-                    pictureBox.Image = new Bitmap($"{partPathImage}_Left_0{extension}");
-                    break;
-                case "D":
-                    pictureBox.Image = new Bitmap($"{partPathImage}_Right_0{extension}");
-                    break;
-                case "W":
-                    if (previousAnimation == AnimationDirection.Left)
-                        pictureBox.Image = new Bitmap($"{partPathImage}_Left_0{extension}");
-                    else if (previousAnimation == AnimationDirection.Right)
-                        pictureBox.Image = new Bitmap($"{partPathImage}_Right_0{extension}");
-                    break;
-                case "S":
-                    if (previousAnimation == AnimationDirection.Left)
-                        pictureBox.Image = new Bitmap($"{partPathImage}_Left_0{extension}");
-                    else if (previousAnimation == AnimationDirection.Right)
-                        pictureBox.Image = new Bitmap($"{partPathImage}_Right_0{extension}");
-                    break;
-            }
-            isPressedAnyKey = false;
-            if (currentAnimation == AnimationDirection.Left || currentAnimation == AnimationDirection.Right)
-                previousAnimation = currentAnimation;
-            currentAnimation = AnimationDirection.None;
-            currentFrame = 0;
+            Paint += new PaintEventHandler(OnPaint);
+            DoubleBuffered = true;
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
         }
 
         private void UpdateAnimation(object sender, EventArgs e)
         {
-            if (isPressedAnyKey) PlayAnimationMovement();
-            if (currentFrame == 3) currentFrame = -1;
-            ++currentFrame;
+            toUpdateAnimation = true;
         }
 
         private void UpdateMovement(object sender, EventArgs e)
         {
-            switch (currentAnimation)
+            switch (wallace.currentAnimation)
             {
                 case AnimationDirection.Left:
-                    pictureBox.Location = new Point(pictureBox.Location.X - 2, pictureBox.Location.Y);
+                    wallace.Left();
                     break;
                 case AnimationDirection.Right:
-                    pictureBox.Location = new Point(pictureBox.Location.X + 2, pictureBox.Location.Y);
+                    wallace.Right();
                     break;
                 case AnimationDirection.Up:
-                    pictureBox.Location = new Point(pictureBox.Location.X, pictureBox.Location.Y - 2);
+                    wallace.Up();
                     break;
                 case AnimationDirection.Down:
-                    pictureBox.Location = new Point(pictureBox.Location.X, pictureBox.Location.Y + 2);
+                    wallace.Down();
                     break;
+                default:
+                    return;
             }
-        }
-
-        private void PlayAnimationMovement()
-        {
-            switch (currentAnimation)
-            {
-                case AnimationDirection.Left:
-                    pictureBox.Image = new Bitmap($"{partPathImage}_Left_{currentFrame}{extension}");
-                    break;
-                case AnimationDirection.Right:
-                    pictureBox.Image = new Bitmap($"{partPathImage}_Right_{currentFrame}{extension}");
-                    break;
-                case AnimationDirection.Up:
-                    if (previousAnimation == AnimationDirection.Left)
-                        pictureBox.Image = new Bitmap($"{partPathImage}_Left_{currentFrame}{extension}");
-                    else if (previousAnimation == AnimationDirection.Right)
-                        pictureBox.Image = new Bitmap($"{partPathImage}_Right_{currentFrame}{extension}");
-                    break;
-                case AnimationDirection.Down:
-                    if (previousAnimation == AnimationDirection.Left)
-                        pictureBox.Image = new Bitmap($"{partPathImage}_Left_{currentFrame}{extension}");
-                    else if (previousAnimation == AnimationDirection.Right)
-                        pictureBox.Image = new Bitmap($"{partPathImage}_Right_{currentFrame}{extension}");
-                    break;
-            }
+            Invalidate();
         }
 
         private void Keybord(object sender, KeyEventArgs e)
@@ -141,19 +76,81 @@ namespace WallaceAndGromit
             switch (e.KeyCode.ToString())
             {
                 case "A":
-                    currentAnimation = AnimationDirection.Left;
+                    wallace.currentAnimation = AnimationDirection.Left;
                     break;
                 case "D":
-                    currentAnimation = AnimationDirection.Right;
+                    wallace.currentAnimation = AnimationDirection.Right;
                     break;
                 case "W":
-                    currentAnimation = AnimationDirection.Up;
+                    wallace.currentAnimation = AnimationDirection.Up;
                     break;
                 case "S":
-                    currentAnimation = AnimationDirection.Down;
+                    wallace.currentAnimation = AnimationDirection.Down;
                     break;
+                default:
+                    return;
             }
             isPressedAnyKey = true;
+        }
+
+        private void FreeKey(object sender, KeyEventArgs e)
+        {
+            isPressedAnyKey = false;
+            wallace.currentFrame = 0;
+            if (wallace.currentAnimation == AnimationDirection.Left
+                || wallace.currentAnimation == AnimationDirection.Right)
+                wallace.previousAnimation = wallace.currentAnimation;
+            wallace.currentAnimation = AnimationDirection.None;
+            Invalidate();
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            Graphics gr = e.Graphics;
+            PlayAnimationMovement(gr);
+        }
+
+        private void PlayAnimationMovement(Graphics gr)
+        {
+            if (isPressedAnyKey)
+            {
+                if (toUpdateAnimation)
+                {
+                    if (wallace.currentFrame == 3) wallace.currentFrame = -1;
+                    ++wallace.currentFrame;
+                    toUpdateAnimation = false;
+                }
+                switch (wallace.currentAnimation)
+                {
+                    case AnimationDirection.Left:
+                        DrawImage(gr, "Left");
+                        break;
+                    case AnimationDirection.Right:
+                        DrawImage(gr, "Right");
+                        break;
+                    case AnimationDirection.Up:
+                        DrawLeftOrRight(gr);
+                        break;
+                    case AnimationDirection.Down:
+                        DrawLeftOrRight(gr);
+                        break;
+                }
+            }
+            else DrawLeftOrRight(gr);
+        }
+
+        private void DrawLeftOrRight(Graphics gr)
+        {
+            if (wallace.previousAnimation == AnimationDirection.Left) DrawImage(gr, "Left");
+            else DrawImage(gr, "Right");
+        }
+
+        private void DrawImage(Graphics gr, string direction)
+        {
+            gr.DrawImage(
+                new Bitmap(
+                    $"{partPathImage}Wallace_{direction}_{wallace.currentFrame}{extension}"),
+                wallace.x, wallace.y, wallace.size.Width, wallace.size.Height);
         }
     }
 }
---
+++ WallaceAndGromit/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WallaceAndGromit
+{
+    class Player
+    {
+        public int x, y;
+        public Size size;
+        public int currentFrame = 0;
+        public AnimationDirection currentAnimation = AnimationDirection.None;
+        public AnimationDirection previousAnimation = AnimationDirection.Right;
+        public int speed;
+        public Player(Size size, int x, int y, int speed)
+        {
+            this.size = size;
+            this.x = x;
+            this.y = y;
+            this.speed = speed;
+        }
+        public void Left()
+        {
+            x -= speed;
+        }
+
+        public void Right()
+        {
+            x += speed;
+        }
+
+        public void Up()
+        {
+            y -= speed;
+        }
+
+        public void Down()
+        {
+            y += speed;
+        }
+    }
+}
