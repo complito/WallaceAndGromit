ce97117fb13edce4b17635a7379e1d74407de66a
complito <hardway186@gmail.com>
2021-04-26 09:32:09 +0500

    Реализована карта

--- WallaceAndGromit/Form1.cs
+++ WallaceAndGromit/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 
 namespace WallaceAndGromit
 {
+
     public enum AnimationDirection
     {
         Left,
@@ -24,6 +25,7 @@ namespace WallaceAndGromit
         private bool isPressedAnyKey = false;
         private bool toUpdateAnimation = false;
         private Player wallace;
+        private Map map;
         private string partPathImage = "D:\\УрФУ\\ЯТП\\WallaceAndGromit\\WallaceAndGromit\\images\\";
         private string extension = ".png";
         private Timer timerAnimation = new Timer { Interval = 100 };
@@ -32,16 +34,45 @@ namespace WallaceAndGromit
         public Form1()
         {
             InitializeComponent();
-            wallace = new Player(new Size(192, 304), 100, 100, 5);
+            DoubleBuffered = true;
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+
+            wallace = new Player(new Size(63, 100), 577, 310, 6);
+            var mapLayout = new int[,] // 0 - grass, 1 - wall
+            {
+                { 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                { 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1}
+            };
+            var grassImage = new Bitmap($"{partPathImage}Grass{extension}");
+            var wallImage = new Bitmap($"{partPathImage}Wall{extension}");
+            map = new Map(mapLayout, 64, 64, grassImage, wallImage);
+
             timerAnimation.Tick += new EventHandler(UpdateAnimation);
             timerAnimation.Start();
             timerMovement.Tick += new EventHandler(UpdateMovement);
             timerMovement.Start();
+
             KeyDown += new KeyEventHandler(Keybord);
             KeyUp += new KeyEventHandler(FreeKey);
             Paint += new PaintEventHandler(OnPaint);
-            DoubleBuffered = true;
-            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
         }
 
         private void UpdateAnimation(object sender, EventArgs e)
@@ -51,23 +82,27 @@ namespace WallaceAndGromit
 
         private void UpdateMovement(object sender, EventArgs e)
         {
-            switch (wallace.currentAnimation)
-            {
-                case AnimationDirection.Left:
-                    wallace.Left();
-                    break;
-                case AnimationDirection.Right:
-                    wallace.Right();
-                    break;
-                case AnimationDirection.Up:
-                    wallace.Up();
-                    break;
-                case AnimationDirection.Down:
-                    wallace.Down();
-                    break;
-                default:
-                    return;
-            }
+                switch (wallace.currentAnimation)
+                {
+                    case AnimationDirection.Left:
+                        if (wallace.x > 64)
+                            wallace.Left();
+                        break;
+                    case AnimationDirection.Right:
+                        if (wallace.x + wallace.size.Width < 1216)
+                            wallace.Right();
+                        break;
+                    case AnimationDirection.Up:
+                        if (wallace.y > 64)
+                            wallace.Up();
+                        break;
+                    case AnimationDirection.Down:
+                        if (wallace.y + wallace.size.Height < 639)
+                            wallace.Down();
+                        break;
+                    default:
+                        return;
+                }
             Invalidate();
         }
 
@@ -107,6 +142,7 @@ namespace WallaceAndGromit
         private void OnPaint(object sender, PaintEventArgs e)
         {
             Graphics gr = e.Graphics;
+            CreateMap(gr);
             PlayAnimationMovement(gr);
         }
 
@@ -147,10 +183,25 @@ namespace WallaceAndGromit
 
         private void DrawImage(Graphics gr, string direction)
         {
-            gr.DrawImage(
-                new Bitmap(
-                    $"{partPathImage}Wallace_{direction}_{wallace.currentFrame}{extension}"),
-                wallace.x, wallace.y, wallace.size.Width, wallace.size.Height);
+            var wallaceImage = new Bitmap($"{partPathImage}Wallace_{direction}_{wallace.currentFrame}{extension}");
+            gr.DrawImage(wallaceImage, wallace.x, wallace.y, wallace.size.Width, wallace.size.Height);
+        }
+
+        private void CreateMap(Graphics gr)
+        {
+            for (var i = 0; i < map.map.GetLength(0); ++i)
+                for (var j = 0; j < map.map.GetLength(1); ++j)
+                    switch (map.map[i, j])
+                    {
+                        case 0: // grass
+                            gr.DrawImage(map.grassImage, i * map.textureWidth, j * map.textureWidth,
+                                map.textureWidth, map.textureHeight);
+                            break;
+                        case 1: // wall
+                            gr.DrawImage(map.wallImage, i * map.textureWidth, j * map.textureWidth,
+                                map.textureWidth, map.textureHeight);
+                            break;
+                    }
         }
     }
 }
---
+++ WallaceAndGromit/Map.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WallaceAndGromit
+{
+    class Map
+    {
+        public int[,] map;
+        public int textureWidth;
+        public int textureHeight;
+        public Image grassImage;
+        public Image wallImage;
+
+        public Map(int[,] map, int textureWidth,
+            int textureHeight, Image grassImage, Image wallImage)
+        {
+            this.map = map;
+            this.textureWidth = textureWidth;
+            this.textureHeight = textureHeight;
+            this.grassImage = grassImage;
+            this.wallImage = wallImage;
+        }
+    }
+}
